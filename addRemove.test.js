81d3ef0fced0b3cdf2dfba8e1fc8ea6bf7b18006
Tugsem <saranlitugsem@gmail.com>
2022-05-05 20:01:41 +0300

    Add new tests

--- addRemove.test.js
+++ addRemove.test.js
@@ -9,15 +9,23 @@ describe('test localStorage add', () => {
     const addAndRemove = new UserTask('Azerty', false, 2);
     expect(addAndRemove.add()).toBe(2);
   });
+  test('Add third sample', () => {
+    const addAndRemove = new UserTask('new test', false, 3);
+    expect(addAndRemove.add()).toBe(3);
+  });
 });
 
 describe('test localStorage remove', () => {
   test('remove first sample', () => {
     const addAndRemove = new UserTask();
-    expect(addAndRemove.removeTask(0)).toBe(2);
+    expect(addAndRemove.removeTask(0)).toBe(3);
   });
-  test('remove first sample', () => {
+  test('remove second sample', () => {
     const addAndRemove = new UserTask();
-    expect(addAndRemove.removeTask(1)).toBe(1);
+    expect(addAndRemove.removeTask(1)).toBe(2);
+  });
+    test('remove third sample', () => {
+    const addAndRemove = new UserTask();
+    expect(addAndRemove.removeTask(2)).toBe(1);
   });
 });
